6f7112b731427adbe9caf4c55762932640c6d661
Jahnavi Reddy <jahnavireddysomu@gmail.com>
2024-07-23 08:29:43 +0530

    TestCommit

---
+++ src/card.js
@@ -0,0 +1,112 @@
+import './card.css';
+function Card(){
+    return (
+        <div className="card-container">
+            <div className="cards">
+                <img className="card-image" src="/profilepic.jpeg" alt="profile" />
+                <div className="name">
+                    <h1 className = "card-title">Scott Ayres</h1>
+                    <p>.2nd</p>
+                </div>
+                <p className = "card-text">Busting social media myths at the Social Media Lab,...</p>
+                <div className ="logo-text-container">
+                    <img src="/profilepic.jpeg" alt="Logo" className="logo" />
+                    <span class="logo-text">23 shared connections</span>
+                </div>
+                <a href="#">Connect</a>
+            </div>
+            <div className="cards">
+                <img className="card-image" src="/profilepic.jpeg" alt="profile" />
+                <div className="name">
+                    <h1 className = "card-title">Richard Beeson</h1>
+                    <p>.2nd</p>
+                </div>
+                <p className = "card-text">SaaS Customer Onboarding Manager/Producer and...</p>
+                <div className ="logo-text-container">
+                    <img src="/profilepic.jpeg" alt="Logo" className="logo" />
+                    <span class="logo-text">7 shared connections</span>
+                </div>
+                <a href="#">Connect</a>
+            </div>
+            <div className="cards">
+                <img className="card-image" src="/profilepic.jpeg" alt="profile" />
+                <div className="name">
+                    <h1 className = "card-title">Jacob Hilpertsh...</h1>
+                    <p>.2nd</p>
+                </div>
+                <p className = "card-text">Harness your social media content and engagement..</p>
+                <div className ="logo-text-container">
+                    <img src="/profilepic.jpeg" alt="Logo" className="logo" />
+                    <span class="logo-text">5 shared connections</span>
+                </div>
+                <a href="#">Connect</a>
+            </div>
+            <div className="cards">
+                <img className="card-image" src="/profilepic.jpeg" alt="profile" />
+                <div className="name">
+                    <h1 className = "card-title">Stephanie Swee...</h1>
+                    <p>.2nd</p>
+                </div>
+                <p className = "card-text">Customer Support/Software QA Tester</p>
+                <div className ="logo-text-container">
+                    <img src="/profilepic.jpeg" alt="Logo" className="logo" />
+                    <span class="logo-text">4 shared connections</span>
+                </div>
+                <a href="#">Connect</a>
+            </div>
+            <div className="cards">
+                <img className="card-image" src="/profilepic.jpeg" alt="profile" />
+                <div className="name">
+                    <h1 className = "card-title">Sarah Hecker</h1>
+                    <p>.2nd</p>
+                </div>
+                <p className = "card-text">US Support & Affiliate Manager at Agorapulse</p>
+                <div className ="logo-text-container">
+                    <img src="/profilepic.jpeg" alt="Logo" className="logo" />
+                    <span class="logo-text">3 shared connections</span>
+                </div>
+                <a href="#">Connect</a>
+            </div>
+            <div className="cards">
+                <img className="card-image" src="/profilepic.jpeg" alt="profile" />
+                <div className="name">
+                    <h1 className = "card-title">Curt Ziegler</h1>
+                    <p>.2nd</p>
+                </div>
+                <p className = "card-text">Customer Support Hero at Agorapulse</p>
+                <div className ="logo-text-container">
+                    <img src="/profilepic.jpeg" alt="Logo" className="logo" />
+                    <span class="logo-text">3 shared connections</span>
+                </div>
+                <a href="#">Connect</a>
+            </div>
+            <div className="cards">
+                <img className="card-image" src="/profilepic.jpeg" alt="profile" />
+                <div className="name">
+                    <h1 className = "card-title">Hannah Recker</h1>
+                    <p>.2nd</p>
+                </div>
+                <p className = "card-text">Inbound & Marketing Operations Manager at...</p>
+                <div className ="logo-text-container">
+                    <img src="/profilepic.jpeg" alt="Logo" className="logo" />
+                    <span class="logo-text">4 shared connections</span>
+                </div>
+                <a href="#">Connect</a>
+            </div>
+            <div className="cards">
+                <img className="card-image" src="/profilepic.jpeg" alt="profile" />
+                <div className="name">
+                    <h1 className = "card-title">Michael Angiletta</h1>
+                    <p>.2nd</p>
+                </div>
+                <p className = "card-text">Growth Marketing | CMO | Agorapulse</p>
+                <div className ="logo-text-container">
+                    <img src="/profilepic.jpeg" alt="Logo" className="logo" />
+                    <span class="logo-text">3 shared connections</span>
+                </div>
+                <a href="#">Connect</a>
+            </div>
+        </div>
+    )
+}
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
-import Review from "./review";
+import Card from "./card";
 // import F1 from "./app";
 const rootElement = document.getElementById('root');
 const rootref = ReactDOM.createRoot(rootElement);
 // rootref.render(<F1/>);
-rootref.render(<Review/>);
+rootref.render(<Card />);
